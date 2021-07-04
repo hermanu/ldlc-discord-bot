fde9f7deeb094adc066509be684c6b5d7219b58a
hermanu <manuguionbajo@gmail.com>
2021-07-04 17:39:39 +0200

    Fixed insultar commands Updated nodemon

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ const client = new Discord.Client();
 require("dotenv").config();
 const prefix = "!";
 
-const commandList = `Comandos disponibles: !insultar !dados`;
+const commandList = `Comandos disponibles: !insultar !dados !pelea`;
 
 //helpers
 const rollDice = require("./helpers/RollDice");
-const insultos = require("./helpers/Insultos");
+const insultos = require("../jmInsultos.json").insultos;
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -18,6 +18,9 @@ client.on("message", (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
+  const taggedUser = message.mentions.users.size
+    ? message.mentions.users.first()
+    : message.author;
 
   // only general chat
   if (message.channel.name === process.env.CHANNEL) {
@@ -27,10 +30,9 @@ client.on("message", (message) => {
     //COMANDO INSULTAR
     if (command === "insultar") {
       if (args[0] === "add") return;
-      const taggedUser = message.mentions.users.size
-        ? message.mentions.users.first()
-        : message.author;
 
+      const ranbdomInsulto =
+        insultos[Math.floor(Math.random() * insultos.length)].toLowerCase();
       message.channel.send(
         `${taggedUser} es un ${insultos[
           Math.floor(Math.random() * insultos.length)
@@ -39,8 +41,16 @@ client.on("message", (message) => {
     }
     //COMMAND DADOS
 
-    if (command == "dados") {
+    if (command === "dados") {
       message.channel.send(`${message.author} ha sacado un ${rollDice(args)}`);
+    }
+
+    //COMMAND FIGHT
+    if (command === "pelea") {
+      const winner =
+        Math.floor(Math.random() * 100 + 1) <= 50 ? message.author : taggedUser;
+      const looser = winner === message.author ? taggedUser : message.author;
+      message.channel.send(`${winner} le ha pegado un palizon a ${looser}`);
     }
   }
 });
